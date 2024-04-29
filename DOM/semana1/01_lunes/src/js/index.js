37c20b7553a88720c46f557f18a77462d41d1b78
devbitan <jquirama96@gmail.com>
2024-04-29 16:22:19 -0500

    lunes

---
+++ DOM/semana1/01_lunes/src/js/index.js
@@ -0,0 +1,21 @@
+// let one = document.getElementById('btn-one')
+// let two = document.getElementsByClassName('btn-two')
+// let three = document.getElementsByName('btn-three')
+// let four = document.getElementsByTagName("button")
+
+// console.log(one);
+// console.log(two);
+// console.log(two[0]);
+// console.log(three);
+// console.log(three[0]);
+// console.log(four);
+// console.log(four[0]);
+// console.log(four[1]);
+
+let one = document.querySelector('button')
+let two = document.querySelectorAll('button')
+let three 
+let four 
+
+console.log(one);
+console.log(two);
