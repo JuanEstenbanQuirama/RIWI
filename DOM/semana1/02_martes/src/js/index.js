5f8e5c80b3107202c048fbc866682b1c60cddde5
devbitan <jquirama96@gmail.com>
2024-04-30 09:25:21 -0500

    clase martes boostrap y dom

---
+++ DOM/semana1/02_martes/src/js/index.js
@@ -0,0 +1,15 @@
+// dos formas de hacer lo mismo 
+// const main = document.getElementsByTagName('main')
+// main[0].appendChild(ramdomImg) 
+// const main = document.querySelector('main')
+// let ramdomImg = document.createElement('img')
+
+// ramdomImg.id = "ramdom"
+// ramdomImg.src = "https://picsum.photos/201"
+// ramdomImg.alt = "ramdom imagen"
+// ramdomImg.width = 100
+// ramdomImg.removeAttribute('id')
+
+// ramdomImg.setAttribute("src", "https://picsum.photos/200") // tambien se peude ramdimgImg.src = "nuevo url"
+// main.appendChild(ramdomImg)
+
